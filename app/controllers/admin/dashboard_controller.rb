ec021301b0e042a81194f13c41a4e15634778142
Charlie Hayes <cjhayestutor@gmail.comgit config --global user.email cjhayestutor@gmail.com>
2020-07-23 01:46:32 +0000

    Added authentication for admin users

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+  # Setting up administrator access:
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+
+  
   def show
   end
 end
