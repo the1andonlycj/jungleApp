b5e9df581c8c8863ea0910b4b59e125b24633289
Charlie Hayes <cjhayestutor@gmail.comgit config --global user.email cjhayestutor@gmail.com>
2020-07-23 14:28:15 +0000

    Implemented basic dashboard features for admins.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,11 @@
 class Admin::DashboardController < ApplicationController
   # Setting up administrator access:
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
-
+  
   
   def show
+    @total_products = Product.count
+    @total_categories = Category.count
   end
+    
 end
