46916e64c9da9263cc03b8f660d63225b52adfd8
Charlie Hayes <cjhayestutor@gmail.comgit config --global user.email cjhayestutor@gmail.com>
2020-07-28 21:29:58 +0000

    Implemented rspec testing for user functionalities and laid the foundations for capybara testing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
+  
+  validates :email, uniqueness: true
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by(email: email.strip)
+    if user && user.authenticate(password)
+      return user
+    else 
+      return nil
+    end
+  end
+  
   has_secure_password
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,91 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# different style of testing here:
+
+  describe "Validations" do
+    it "should create a valid user" do
+      user = User.create(
+        first_name: "Bobbert",
+        last_name: "Bobbins",
+        email: "bobbert@test.com",
+        password: "testy",
+        password_confirmation: "testy"
+        )
+        expect(user).to be_valid
+    end
+
+    it "should have matching password and password confirmation inputs" do
+      user = User.create(
+        first_name: "Bobbert",
+        last_name: "Bobbins",
+        email: "bobbert@test.com",
+        password: "testy",
+        password_confirmation: "testy"
+        )
+      expect(user.password).to eq(user.password_confirmation)
+    end
+
+    it "should not create an user if the email exists" do
+      user1 = User.create(
+        first_name: "Bobbert",
+        last_name: "Bobbins",
+        email: "bobbert@test.com",
+        password: "testy",
+        password_confirmation: "testy"
+        )
+      user2 = User.create(
+        first_name: "Bobbert",
+        last_name: "Bobbins",
+        email: "bobbert@test.com",
+        password: "testy",
+        password_confirmation: "testy"
+        )
+      # check database for matching 
+      # expect(user1.valid?).to eq(true)
+      expect(user2.valid?).to eq(false)
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    it "returns user if it exists" do
+      user1 = User.create(
+        first_name: "Bobbert",
+        last_name: "Bobbins",
+        email: "bobbert@test.com",
+        password: "testy",
+        password_confirmation: "testy"
+        )
+      user2 = User.authenticate_with_credentials("bobbert@test.com", "testy")
+      expect(user2).to eq(user1)
+    end
+
+    it "doesn't return user if password does not match" do
+      user1 = User.create(
+        first_name: "Bobbert",
+        last_name: "Bobbins",
+        email: "bobbert@test.com",
+        password: "testy",
+        password_confirmation: "testy"
+        )
+      user2 = User.authenticate_with_credentials("bobbert@test.com", "12testy")
+      expect(user2).to eq(nil)
+    end
+
+    it "logs in even if there's whitespace around the user email" do
+      user1 = User.create(
+        first_name: "Bobbert",
+        last_name: "Bobbins",
+        email: "  bobbert@test.com  ",
+        password: "testy",
+        password_confirmation: "testy"
+        )
+      expect(user1.valid?).to eq(true)
+    end
+  end
 end
+
+
+
+# when checking email, we should automatically downcase it: .downcase so we 
+# can compare it all without worrying about whether or not the case matches.
