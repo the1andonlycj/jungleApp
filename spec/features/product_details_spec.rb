672ce73f65e9cefe146a638bcf52746c1bb02f88
Charlie Hayes <cjhayestutor@gmail.comgit config --global user.email cjhayestutor@gmail.com>
2020-07-28 23:26:07 +0000

    Added capybara test to check if a user can visit the home page, then click on details for an item and successfully load the new page.

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They come to the homepage and then click on a product" do
+    # ACT
+    visit root_path
+    first('.product').click_link('Details')
+
+    # DEBUG / VERIFY
+    expect(page).to have_css('.products-show')
+    save_screenshot
+    
+  end
+end
+
+
+
+# in case i can't click the button, maybe i will use this:
+# "btn btn-default pull-right"
